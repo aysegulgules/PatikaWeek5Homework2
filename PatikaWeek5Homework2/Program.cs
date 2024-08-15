2934f63cddd63b6c27c247262dd3fd45cc836233
aysegulgules <159700271+aysegulgules@users.noreply.github.com>
2024-08-15 10:12:48 +0300

    Add project files.

---
+++ PatikaWeek5Homework2/Program.cs
@@ -0,0 +1,27 @@
+﻿
+// string tipinde liste tanımlanıyor..
+List<string> guests=new List<string>();
+
+//ziyaretçi listesine bilgiler giriliyor..
+while (true)
+{
+    Console.WriteLine("\nZiyaretçi ad-soyad giriniz veya işlemden çıkmak için 'bitti' yazınız..:");
+    string guest = Console.ReadLine();
+    if (guest == "bitti")
+    {
+        break;
+    }
+    else
+    {
+        guests.Add(guest);
+    }
+}
+
+//Ziyaretçiler ekrana yazdırılıyor...
+
+Console.WriteLine("\nZiyaretçiler--------");
+
+foreach(string guest  in guests)
+{
+    Console.WriteLine($"{guest}");
+}
